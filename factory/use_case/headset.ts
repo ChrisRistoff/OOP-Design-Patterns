2271e7072481ce2d1a5aba02a5a9875e4833e2c6
Krasyo <doanusho@gmail.com>
2024-03-08 10:44:29 +0000

    Add factory use case

---
+++ factory/use_case/headset.ts
@@ -0,0 +1,46 @@
+import { stats } from './stats_type';
+
+interface IHeadset {
+  id: number,
+  brand: string,
+  model: string,
+  price: number,
+  microphone: boolean,
+  noise_cancelling: boolean,
+  wireless: boolean,
+  bluetooth: boolean,
+  battery_life: number,
+  getStats(): stats;
+}
+
+export default class Headset implements IHeadset {
+  constructor(
+    public id: number,
+    public brand: string,
+    public model: string,
+    public price: number,
+    public microphone: boolean,
+    public noise_cancelling: boolean,
+    public wireless: boolean,
+    public bluetooth: boolean,
+    public battery_life: number
+  ) {}
+
+  create(): IHeadset {
+    return this
+  }
+
+  getStats(): stats {
+    return {
+      id: this.id,
+      brand: this.brand,
+      model: this.model,
+      price: this.price,
+      microphone: this.microphone,
+      noise_cancelling: this.noise_cancelling,
+      wireless: this.wireless,
+      bluetooth: this.bluetooth,
+      battery_life: this.battery_life
+    };
+  }
+}
---
+++ factory/use_case/stats_type.ts
@@ -0,0 +1,11 @@
+export type stats = {
+  id: number,
+  brand: string,
+  model: string,
+  price: number,
+  microphone: boolean,
+  noise_cancelling: boolean,
+  wireless: boolean,
+  bluetooth: boolean,
+  battery_life: number,
+}
